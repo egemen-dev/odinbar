8f242368f956c538fcbc4bf0ffed34a3ee41166c
Egemen <egemenwasd@gmail.com>
2022-06-23 23:27:55 +0300

    clean up the codes that repeats in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :username, uniqueness: true
-  validates :email, uniqueness: true
-  validates_presence_of :email
-  validates_presence_of :username
+  validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
