9a5ef7922d6696563af0df1a6293b6691e1f983b
Egemen <egemenwasd@gmail.com>
2022-06-24 17:33:17 +0300

    relations and validations are added to Friendship model - connecting with User model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,2 +1,17 @@
 class Friendship < ApplicationRecord
+  # Relations
+  belongs_to :user
+  belongs_to :friend, class_name: 'User', foreign_key: 'friend_id'
+
+  # Validations
+  validates :user_id, presence: true
+  validates :friend_id, presence: true
+  validate :user_and_friend_are_not_the_same
+  validates_uniqueness_of :user_id, scope: [:user_id]
+
+  def user_and_friend_are_not_the_same
+    if friend == user
+      errors.add(:friendship, "can't be friends with same account")
+    end
+  end
 end
