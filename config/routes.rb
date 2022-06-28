eb52878d49ec4eeed5ec2422711504d357fa30ea
Egemen <egemenwasd@gmail.com>
2022-06-28 22:41:21 +0300

    root added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :friendships
+  resources :profiles, only: [:show, :index]
+  root 'profiles#index'
   devise_for :users
+  resources :friendships
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
