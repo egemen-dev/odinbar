ab73afd03b444d17e4ecc17e500fe0837ede29f9
Egemen <egemenwasd@gmail.com>
2022-06-30 22:50:41 +0300

    friendship update and destroy methods are updated

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -19,48 +19,53 @@ class FriendshipsController < ApplicationController
   def edit
   end
 
+  def accept
+  end
+
   # POST /friendships or /friendships.json
   def create
     @friendship = current_user.friendships.new(friend_id: params[:friend_id])
-      if @friendship.save
-        flash[:notice] = 'Friendship Request was successfully created.'
-        redirect_to friendship_path(@friendship)
-      else
-        flash[:alert] = 'Something went wrong, try again later.'
-      end
+    if @friendship.save
+      flash[:notice] = 'Friendship Request was successfully created.'
+    else
+      flash[:alert] = 'Something went wrong, try again later.'
+    end
+    redirect_to users_sent_requests_path(current_user.id)
   end
 
   # PATCH/PUT /friendships/1 or /friendships/1.json
   def update
-    respond_to do |format|
-      if @friendship.update(friendship_params)
-        format.html { redirect_to friendship_url(@friendship), notice: "Friendship was successfully updated." }
-        format.json { render :show, status: :ok, location: @friendship }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @friendship.errors, status: :unprocessable_entity }
-      end
+    @friendship = set_friendship
+    if @friendship.update_attribute('status', params[:status])
+      flash[:notice] = 'Friendship Request was successfully accepted.'
+      redirect_to users_friends_path(current_user.id)
+    else
+      flash[:alert] = 'Something went wrong, try again later.'
+      redirect_to users_friends_path(current_user.id)
     end
   end
 
   # DELETE /friendships/1 or /friendships/1.json
   def destroy
+    @friendship = set_friendship
     @friendship.destroy
-
-    respond_to do |format|
-      format.html { redirect_to friendships_url, notice: "Friendship was successfully destroyed." }
-      format.json { head :no_content }
+    if @friendship.destroy
+      flash[:notice] = 'Friendship Request was successfully destroyed.'
+      redirect_to users_profile_path(current_user.id)
+    else
+      flash[:alert] = 'Something went wrong, try again later.'
+      redirect_to users_profile_path(current_user.id)
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_friendship
-      @friendship = Friendship.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_friendship
+    @friendship = Friendship.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def friendship_params
-      params.require(:friendship).permit(:friend_id)
-    end
+  # Only allow a list of trusted parameters through.
+  def friendship_params
+    params.require(:friendship).permit(:friend_id, :status, :sender_id)
+  end
 end
