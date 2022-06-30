386cf3df1fe7692576b074d4fc44cf08fedfbfa4
Egemen <egemenwasd@gmail.com>
2022-06-30 22:56:07 +0300

    new routes are added to manage users friendship requests and profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :profiles, only: [:show, :index]
-  root 'profiles#index'
+  # resources :profiles, only: [:show, :index]
+  root 'users#index'
   devise_for :users
   resources :friendships
 
-  get 'users/friends' => 'users#friends', :as => :users_friends
-  get 'users/received_requests' => 'users#received_requests', :as => :users_received_requests
-  get 'users/sent_requests' => 'users#sent_requests', :as => :users_sent_requests
+  get 'users/all' => 'users#index', :as => :users_index
+  get 'users/:id/profile' => 'users#show', :as => :users_profile
+  get 'users/:id/friends' => 'users#friends', :as => :users_friends
+  get 'users/:id/received_requests' => 'users#received_requests', :as => :users_received_requests
+  get 'users/:id/sent_requests' => 'users#sent_requests', :as => :users_sent_requests
 
 end
