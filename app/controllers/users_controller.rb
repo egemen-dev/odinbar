dcf3c4406da42ba85b4b3f102832a3e94f0f0b09
Egemen <egemenwasd@gmail.com>
2022-06-30 22:54:42 +0300

    user controller - new methods added to see the firendship of the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,41 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   # Friendship Requests Methods
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+
   def friends
-    @friends = current_user.active_friends
+    @user = User.find(params[:id])
+    if @user == current_user
+      @friends = current_user.active_friends
+    else
+      redirect_to root_path
+      flash[:alert] = "You don't have a permission to see #{@user.username}'s friendships page."
+    end
   end
 
   def received_requests
-    @received_requests = current_user.received_requests
+    @user = User.find(params[:id])
+    if @user == current_user
+      @received_requests = current_user.received_requests
+    else
+      redirect_to root_path
+      flash[:alert] = "You don't have a permission to see #{@user.username}'s friendship requests page."
+    end
   end
 
   def sent_requests
-    @sent_requests = current_user.sent_requests
+    @user = User.find(params[:id])
+    if @user == current_user
+      @sent_requests = current_user.sent_requests
+    else
+      redirect_to root_path
+      flash[:alert] = "You don't have a permission to see #{@user.username}'s friendship requests page."
+    end
   end
 end
