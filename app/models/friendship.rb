b941a64d9256512c849f378840753e9c0fb06196
Egemen <egemenwasd@gmail.com>
2022-07-01 09:15:44 +0300

    friendship model - new validation added to prevent having multiple friendships with same person

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -6,8 +6,16 @@ class Friendship < ApplicationRecord
   # Validations
   validates :user_id, presence: true
   validates :friend_id, presence: true
+  validate :user_and_friend_are_not_already_friends
   validate :user_and_friend_are_not_the_same
   validates_uniqueness_of :user_id, scope: [:friend_id]
+
+  def user_and_friend_are_not_already_friends
+    unless Friendship.where(user_id: user.id, friend_id: friend.id).empty? && \
+           Friendship.where(user_id: friend.id, friend_id: user.id).empty?
+      errors.add(:friendship, 'user and friend already have a mutual friendship')
+    end
+  end
 
   def user_and_friend_are_not_the_same
     if friend == user
