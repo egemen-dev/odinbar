4e170cafa92f5e7539045fe7142555d5fce5b7c8
Egemen <egemenwasd@gmail.com>
2022-07-03 18:50:04 +0300

    vanilla javascript added to toggle navbar menu in mobile view

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+var navbarMenu = document.querySelector('.navbar-menu');
+var navbarBurger = document.querySelector('.navbar-burger');
+navbarBurger.addEventListener('click', () => {
+  navbarMenu.classList.toggle('is-active')
+});
