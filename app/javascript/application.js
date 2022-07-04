a4fe18f09f12392dd8513cc6a67560160a81e612
Egemen <egemenwasd@gmail.com>
2022-07-04 11:33:37 +0300

    js file called 2 times fixed

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-var navbarMenu = document.querySelector('.navbar-menu');
-var navbarBurger = document.querySelector('.navbar-burger');
+// import "@hotwired/turbo-rails"
+// import "controllers"
+
+const navbarMenu = document.getElementsByClassName('navbar-menu')[0]
+const navbarBurger = document.getElementsByClassName('navbar-burger')[0]
+
 navbarBurger.addEventListener('click', () => {
-  navbarMenu.classList.toggle('is-active')
+  navbarBurger.classList.toggle('is-active');
+  navbarMenu.classList.toggle('is-active');
 });
