b558be3502ddf2cf0d1c50c79b0c0224890eb0a5
Egemen <egemenwasd@gmail.com>
2022-07-04 16:07:38 +0300

    frienship model validate method (user_and_friend_are_not_already_friends) updated

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -11,6 +11,8 @@ class Friendship < ApplicationRecord
   validates_uniqueness_of :user_id, scope: [:friend_id]
 
   def user_and_friend_are_not_already_friends
+    return false if user.nil? || friend.nil?
+
     unless Friendship.where(user_id: user.id, friend_id: friend.id).empty? && \
            Friendship.where(user_id: friend.id, friend_id: user.id).empty?
       errors.add(:friendship, 'user and friend already have a mutual friendship')
