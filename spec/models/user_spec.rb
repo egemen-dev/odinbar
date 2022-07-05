684fa27c2d85d50e9690a5c3c39858afb2776596
Egemen <egemenwasd@gmail.com>
2022-07-05 18:44:38 +0300

    Refactor user spec for readability

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,40 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "Associations with User" do
+  before(:each) do
+    @user1 = User.find_by(username: 'TesterJoe', email: 'testerjoe@gmail.com') || FactoryBot.build(:user)
+  end
+
+  describe "Associations with other Users" do
     it { should have_many(:friendships).without_validating_presence }
   end
 
-  before(:all) do
-    @user1 = User.find_by(username: 'TesterJoe', email: 'testerjoe@gmail.com') || create(:user)
-  end
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      expect(@user1).to be_valid
+    end
 
-  it 'is valid with valid attributes' do
-    expect(@user1).to be_valid
-  end
+    it 'has a unique username' do
+      create(:user, username: 'TesterJoe')
+      user2 = build(:user, email: 'abctest@gmail.com')
+      expect(user2).to_not be_valid
+    end
 
-  it 'has a unique username' do
-    user2 = build(:user, email: 'abctest@gmail.com')
-    expect(user2).to_not be_valid
-  end
+    it 'has a unique email' do
+      create(:user, email: 'testerjoe@gmail.com')
+      user2 = build(:user, username: 'Joe', email: 'testerjoe@gmail.com')
+      expect(user2).to_not be_valid
+    end
 
-  it 'has a unique email' do
-    user2 = build(:user, username: 'Joe')
-    expect(user2).to_not be_valid
-  end
+    it 'is not valid without a password' do
+      user2 = build(:user, password: nil)
+      expect(user2).to_not be_valid
+    end
 
-  it 'is not valid without a password' do
-    user2 = build(:user, password: nil)
-    expect(user2).to_not be_valid
-  end
+    it 'is not valid without a username' do
+      user2 = build(:user, username: nil)
+      expect(user2).to_not be_valid
+    end
 
-  it 'is not valid without a username' do
-    user2 = build(:user, username: nil)
-    expect(user2).to_not be_valid
-  end
-
-  it 'is not valid without an email' do
-    user2 = build(:user, email: nil)
-    expect(user2).to_not be_valid
+    it 'is not valid without an email' do
+      user2 = build(:user, email: nil)
+      expect(user2).to_not be_valid
+    end
   end
 end
