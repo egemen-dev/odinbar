05981d60b44c278a6dfd3c2a721522faf7e51ca5
Egemen <egemenwasd@gmail.com>
2022-07-06 12:14:31 +0300

    Add resources block
    I added users block beacuse every route was added separately at first
    it's better for reading now and easier to add new routes inside the block

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
-  # resources :profiles, only: [:show, :index]
   root 'users#index'
   devise_for :users
-  resources :friendships
+  
+  resources :friendships, only: [:create, :update, :destroy]
 
-  get 'users/all' => 'users#index', :as => :users_index
-  get 'users/:id/profile' => 'users#show', :as => :users_profile
-  get 'users/:id/friends' => 'users#friends', :as => :users_friends
-  get 'users/:id/received_requests' => 'users#received_requests', :as => :users_received_requests
-  get 'users/:id/sent_requests' => 'users#sent_requests', :as => :users_sent_requests
+  resources :users, only: [:index] do
+    get 'show', :as => :profile
+    get 'friends'
+    get 'received_requests'
+    get 'sent_requests'
+  end
 
+  # get 'users/all' => 'users#index', :as => :users_index
+  # get 'users/:user_id/profile' => 'users#show', :as => :users_profile
+  # get 'users/:user_id/friends' => 'users#friends', :as => :users_friends
+  # get 'users/:user_id/received_requests' => 'users#received_requests', :as => :users_received_requests
+  # get 'users/:user_id/sent_requests' => 'users#sent_requests', :as => :users_sent_requests
 end
