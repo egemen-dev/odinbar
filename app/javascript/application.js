c6393b6083c5c057f733bda75cee3cbe581173f1
Egemen <egemenwasd@gmail.com>
2022-07-06 21:08:59 +0300

    Add timeout function to remove flash messages
    I added this for flash messages to disappear after 5 sec.
    Before, it was at the screen until the refresh.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -9,3 +9,11 @@ navbarBurger.addEventListener('click', () => {
   navbarBurger.classList.toggle('is-active');
   navbarMenu.classList.toggle('is-active');
 });
+
+const flashMessage = document.getElementById('flash-messages')
+if (flashMessage != null) {
+  setTimeout(function removeFlash() {
+    flashMessage.remove();
+    console.log(flashMessage);
+  }, 5000)
+}
