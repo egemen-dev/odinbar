ee035e92095b73286c0b5a99c5f676a93dc082ae
Egemen <egemenwasd@gmail.com>
2022-07-17 21:41:08 +0300

    Update integration tests
    HTML forms are updated, so tests needed a minor update.
    Like, finding the password input section in the page.

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'User Sign Up', type: :feature do
     fill_in 'Username', with: 'user1'
     fill_in 'Email', with: 'user1@mail.com'
     fill_in 'Password', with: '123456'
-    fill_in 'Password confirmation', with: '123456'
+    fill_in 'user_password_confirmation', with: '123456'
     click_on 'Sign up'
     expect(page).to have_content('Welcome! You have signed up successfully.')
   end
