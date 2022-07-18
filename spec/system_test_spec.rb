183694bca322e11adf8ef168bf4c37161d2cd7b7
Egemen <egemenwasd@gmail.com>
2022-07-18 19:15:12 +0300

    Update system test
    Updated to test Post model - controller - views etc.

--- spec/system_test_spec.rb
+++ spec/system_test_spec.rb
@@ -94,4 +94,48 @@ RSpec.describe 'User Actions', type: :system do
       expect(page).to have_content("You don't have a permission to see #{user2.username}'s friendship requests page.")
     end
   end
+
+  context 'Create - Edit - Delete a post' do
+    it 'user creates a posts, updates and then deletes it' do
+      login_as(FactoryBot.create(:user))
+      user2 = FactoryBot.create(:user, email: 'user1@mail.com', username: 'user1')
+      visit root_path
+
+      # Create false post
+      click_on 'New Post'
+      fill_in 'post_body', with: ''
+      click_on 'Create Post'
+      expect(page).to have_content("Body can't be blank")
+
+      # Create post
+      click_on 'New Post'
+      fill_in 'post_body', with: 'This is odinbar!'
+      click_on 'Create Post'
+      expect(page).to have_content('Post successfully created.')
+
+      # Edit false post
+      click_on 'This is odinbar!'
+      expect(page).to have_content('This is odinbar!')
+      click_on 'Edit Post'
+      fill_in 'post_body', with: ''
+      click_on 'Update Post'
+      expect(page).to have_content("Body can't be blank")
+
+      # Edit post
+      click_on 'Flow'
+      click_on 'This is odinbar!'
+      expect(page).to have_content('This is odinbar!')
+      click_on 'Edit Post'
+      fill_in 'post_body', with: 'This post is EDITED!!!'
+      click_on 'Update Post'
+      expect(page).to have_content('Post successfully updated.')
+
+      # Delete post
+      click_on 'Flow'
+      expect(page).to have_content('This post is EDITED!!!')
+      click_on 'This post is EDITED!!!'
+      click_on 'Delete'
+      expect(page).to have_content('Post successfully deleted.')
+    end
+  end
 end
