a78e11e3599a0bc0b0f54f2bf771c07081089c6c
Egemen <egemenwasd@gmail.com>
2022-07-19 16:59:53 +0300

    Add after filter to FriendshipsController
    Added to create notifications relevant to actions.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,7 @@
 class FriendshipsController < ApplicationController
   before_action :authenticate_user!
+  after_action :create_request_notification, only: [:create]
+  after_action :create_acceptance_notification, only: [:update]
 
   def create
     @friendship = current_user.friendships.new(friend_id: params[:friend_id])
@@ -35,5 +37,16 @@ class FriendshipsController < ApplicationController
   private
   def set_friendship
     @friendship = Friendship.find(params[:id])
+  end
+
+  def create_request_notification
+    @notification = Notification.create(user_id: params[:friend_id],
+       body:"You have a new friendship request from #{current_user.username}")
+  end
+
+  def create_acceptance_notification
+    @friendship = set_friendship
+    @notification = Notification.create(user_id: @friendship.user.id,
+      body:"#{@friendship.friend.username} accepted your friendship request.")
   end
 end
