b65b52e0ee3283971af88353bd07e1713a542a8b
Egemen <egemenwasd@gmail.com>
2022-07-22 17:24:17 +0300

    Add redirect_to in post_likings_controller.rb

--- app/controllers/post_likings_controller.rb
+++ app/controllers/post_likings_controller.rb
@@ -4,11 +4,13 @@ class PostLikingsController < ApplicationController
   def create
     if PostLiking.where(user: current_user, post_id:params[:post_id]).empty?
       @post_liking = PostLiking.create(user: current_user, post_id:params[:post_id])
+      redirect_to @post_liking.post
     end
   end
 
   def destroy
     @post_liking = PostLiking.find_by(user: current_user, post_id:params[:id])
     @post_liking.destroy
+    redirect_to @post_liking.post
   end
 end
