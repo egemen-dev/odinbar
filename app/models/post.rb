1a6ebc196ead70c7b59c0b8a70f2737696a5f541
Egemen <egemenwasd@gmail.com>
2022-07-23 19:55:14 +0300

    Add has_rich_text to post model
    This is necessary for trix to work

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
-  validates :body, presence: true, length: { in: 1..1000 }
+  validates :body, presence: true
+  has_rich_text :body
 
   has_many :post_likings, dependent: :destroy
   has_many :user_likes, through: :post_likings, source: :user
