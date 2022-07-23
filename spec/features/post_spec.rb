cc238cef1aa1a10f621ed03fb822fb43bf264823
Egemen <egemenwasd@gmail.com>
2022-07-23 20:29:06 +0300

    Update post_spec test
    The form has changed due to the rich_text_area.I updated the post spec to fill in the form properly.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -8,30 +8,30 @@ RSpec.describe 'Post Spec', type: :feature do
 
     # Create false post
     click_on 'New Post'
-    fill_in 'post_body', with: ''
+    find_field(id: 'post_body_trix_input_post', type: :hidden).set(' ')
     click_on 'Create Post'
     expect(page).to have_content("Body can't be blank")
-
+    
     # Create post
     click_on 'New Post'
-    fill_in 'post_body', with: 'This is odinbar!'
+    find_field(id: 'post_body_trix_input_post', type: :hidden).set('This is odinbar!')
     click_on 'Create Post'
     expect(page).to have_content('This is odinbar!')
-
+    
     # Edit false post
     click_on 'This is odinbar!'
     expect(page).to have_content('This is odinbar!')
     click_on 'Edit Post'
-    fill_in 'post_body', with: ''
+    find_field(id: 'post_body_trix_input_post_1', type: :hidden).set('  ')
     click_on 'Update Post'
     expect(page).to have_content("Body can't be blank")
-
+    
     # Edit post
     click_on 'Flow'
     click_on 'This is odinbar!'
     expect(page).to have_content('This is odinbar!')
     click_on 'Edit Post'
-    fill_in 'post_body', with: 'This post is EDITED!!!'
+    find_field(id: 'post_body_trix_input_post_1', type: :hidden).set('This post is EDITED!!!')
     click_on 'Update Post'
     expect(page).to have_content('This post is EDITED!!!')
 
