bf1ceed9a0042d35cd06a37dafedad211359c1c4
Egemen <egemenwasd@gmail.com>
2022-07-30 10:21:37 +0300

    Setup Omniauth to use Google

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,5 +49,21 @@ class User < ApplicationRecord
 
   # Devise
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.from_omniauth(access_token)
+  data = access_token.info
+  user = User.where(email: data['email']).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    unless user
+        user = User.create(username: data['name'],
+            email: data['email'],
+            password: Devise.friendly_token[0,20],
+            avatar_url: data['image']
+        )
+    end
+    user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :comments
   root 'users#index'
-  devise_for :users
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   
   resources :friendships, only: [:create, :update, :destroy]
 
