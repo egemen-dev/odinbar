7a7eb50d70201165f620acc4b75ab992b8b8b472
Egemen <egemenwasd@gmail.com>
2022-07-30 18:07:12 +0300

    Zeitwerk::NameError when deploying heroku

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Odinbar
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
+    config.autoloader = :classic
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
