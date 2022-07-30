2673e6ccd0d0288fd9b5cfff0f17e9f55895ff22
Egemen <egemenwasd@gmail.com>
2022-07-30 10:29:30 +0300

    Add avatar_url column to user model
    When signing up with Google, retrive
    the gmail profile image for the user.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_26_055545) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_30_065417) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -99,6 +99,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_26_055545) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "avatar_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
