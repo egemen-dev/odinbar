9086e31f61c47edad66f183ffdb510db9bf3aec9
Egemen <egemenwasd@gmail.com>
2022-08-04 23:23:17 +0300

    Redirect after cancel account

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,13 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   
+  def destroy
+    resource.destroy
+    redirect_to new_user_registration_path
+  end
 
   protected
 
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
-
 end
