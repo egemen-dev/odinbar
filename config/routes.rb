8b5e9792ab3d4360947a052296555f5c2ecfccef
Egemen <egemenwasd@gmail.com>
2022-08-04 16:11:29 +0300

    Create devise registartion controller to override
    To be able to update user without password

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :comments
   root 'users#index'
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { 
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
   
   resources :friendships, only: [:create, :update, :destroy]
 
